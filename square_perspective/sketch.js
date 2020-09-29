458c0d94c74ba7e64610dc92c7b2356953e9bf0b
BENJAMIN CEA <benjaminceavidal@gmail.com>
2020-09-29 17:20:09 -0500

    color patern with squares; grayscale

---
+++ square_perspective/sketch.js
@@ -0,0 +1,84 @@
+function setup() {
+  createCanvas(450, 450);
+}
+
+function draw() {
+  background(0);
+  
+  rectMode(RADIUS);
+  fill(30);
+  rect(225, 225, 210, 210);
+  
+  rectMode(RADIUS);
+  fill(70);
+  rect(225, 225, 205, 205);
+  
+  rectMode(RADIUS);
+  fill(110);
+  rect(225, 225, 200, 200);
+  
+  rectMode(RADIUS);
+  fill(30);
+  rect(225, 225, 195, 195);
+  
+  rectMode(RADIUS);
+  fill(60);
+  rect(225, 225, 185, 185);
+  
+  rectMode(RADIUS);
+  fill(110);
+  rect(225, 225, 175, 175);
+  
+  rectMode(RADIUS);
+  fill(160);
+  rect(225, 225, 165, 165);
+  
+  rectMode(RADIUS);
+  fill(80);
+  rect(225, 225, 155, 155);
+  
+  rectMode(RADIUS);
+  fill(120);
+  rect(225, 225, 145, 145);
+  
+  rectMode(RADIUS);
+  fill(160);
+  rect(225, 225, 135, 135);
+  
+  rectMode(RADIUS);
+  fill(200);
+  rect(225, 225, 125, 125);
+  
+  rectMode(RADIUS);
+  fill(140);
+  rect(225, 225, 115, 115);
+  
+  rectMode(RADIUS);
+  fill(170);
+  rect(225, 225, 105, 105);
+  
+  rectMode(RADIUS);
+  fill(200);
+  rect(225, 225, 95, 95);
+  
+  rectMode(RADIUS);
+  fill(230);
+  rect(225, 225, 85, 85);
+  
+  rectMode(RADIUS);
+  fill(190);
+  rect(225, 225, 75, 75);
+  
+  rectMode(RADIUS);
+  fill(210);
+  rect(225, 225, 65, 65);
+  
+  rectMode(RADIUS);
+  fill(230);
+  rect(225, 225, 55, 55);
+  
+  rectMode(RADIUS);
+  fill(250);
+  rect(225, 225, 45, 45);
+  
+}

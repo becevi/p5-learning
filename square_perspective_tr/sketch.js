fcf5f8711cb3a3c2827b0c2c6d7c90ad9dc12f30
BENJAMIN CEA <benjaminceavidal@gmail.com>
2020-09-29 19:45:01 -0500

    Pattern with trans flag colors

---
+++ square_perspective_tr/sketch.js
@@ -0,0 +1,84 @@
+function setup() {
+  createCanvas(450, 450);
+}
+
+function draw() {
+  background(250, 0, 130);
+  
+  rectMode(RADIUS);
+  fill(250, 250, 250);
+  rect(225, 225, 210, 210);
+  
+  rectMode(RADIUS);
+  fill(250, 0, 145);
+  rect(225, 225, 205, 205);
+  
+  rectMode(RADIUS);
+  fill(70, 0, 240);
+  rect(225, 225, 200, 200);
+  
+  rectMode(RADIUS);
+  fill(250, 250, 250);
+  rect(225, 225, 195, 195);
+  
+  rectMode(RADIUS);
+  fill(70, 0, 240);
+  rect(225, 225, 185, 185);
+  
+  rectMode(RADIUS);
+  fill(250, 0, 140);
+  rect(225, 225, 175, 175);
+  
+  rectMode(RADIUS);
+  fill(250, 250, 250);
+  rect(225, 225, 165, 165);
+  
+  rectMode(RADIUS);
+  fill(250, 0, 140);
+  rect(225, 225, 155, 155);
+  
+  rectMode(RADIUS);
+  fill(60, 0, 240);
+  rect(225, 225, 145, 145);
+  
+  rectMode(RADIUS);
+  fill(250, 250, 250);
+  rect(225, 225, 135, 135);
+  
+  rectMode(RADIUS);
+  fill(60, 0, 240);
+  rect(225, 225, 125, 125);
+  
+  rectMode(RADIUS);
+  fill(250, 0, 135);
+  rect(225, 225, 115, 115);
+  
+  rectMode(RADIUS);
+  fill(250, 250, 250);
+  rect(225, 225, 105, 105);
+  
+  rectMode(RADIUS);
+  fill(250, 0, 135);
+  rect(225, 225, 95, 95);
+  
+  rectMode(RADIUS);
+  fill(50, 0, 240);
+  rect(225, 225, 85, 85);
+  
+  rectMode(RADIUS);
+  fill(250, 250, 250);
+  rect(225, 225, 75, 75);
+  
+  rectMode(RADIUS);
+  fill(50, 0, 240);
+  rect(225, 225, 65, 65);
+  
+  rectMode(RADIUS);
+  fill(250, 0, 130);
+  rect(225, 225, 55, 55);
+  
+  rectMode(RADIUS);
+  fill(250, 250, 250);
+  rect(225, 225, 45, 45);
+  
+}

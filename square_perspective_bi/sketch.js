101e228159ecb0f43c896acf93ff9d31f9d30fa0
BENJAMIN CEA <benjaminceavidal@gmail.com>
2020-09-29 19:43:51 -0500

    Pattern with bi colors

---
+++ square_perspective_bi/sketch.js
@@ -0,0 +1,84 @@
+function setup() {
+  createCanvas(450, 450);
+}
+
+function draw() {
+  background(250, 0, 120);
+  
+  rectMode(RADIUS);
+  fill(200, 0, 120);
+  rect(225, 225, 210, 210);
+  
+  rectMode(RADIUS);
+  fill(250, 0, 130);
+  rect(225, 225, 205, 205);
+  
+  rectMode(RADIUS);
+  fill(240, 0, 120);
+  rect(225, 225, 200, 200);
+  
+  rectMode(RADIUS);
+  fill(250, 0, 120);
+  rect(225, 225, 195, 195);
+  
+  rectMode(RADIUS);
+  fill(230, 0, 120);
+  rect(225, 225, 185, 185);
+  
+  rectMode(RADIUS);
+  fill(220, 0, 120);
+  rect(225, 225, 175, 175);
+  
+  rectMode(RADIUS);
+  fill(170, 0, 120);
+  rect(225, 225, 165, 165);
+  
+  rectMode(RADIUS);
+  fill(200, 0, 120);
+  rect(225, 225, 155, 155);
+  
+  rectMode(RADIUS);
+  fill(230, 0, 120);
+  rect(225, 225, 145, 145);
+  
+  rectMode(RADIUS);
+  fill(250, 0, 130);
+  rect(225, 225, 135, 135);
+  
+  rectMode(RADIUS);
+  fill(150, 0, 130);
+  rect(225, 225, 125, 125);
+  
+  rectMode(RADIUS);
+  fill(180, 0, 130);
+  rect(225, 225, 115, 115);
+  
+  rectMode(RADIUS);
+  fill(200, 0, 120);
+  rect(225, 225, 105, 105);
+  
+  rectMode(RADIUS);
+  fill(150, 0, 130);
+  rect(225, 225, 95, 95);
+  
+  rectMode(RADIUS);
+  fill(130, 0, 120);
+  rect(225, 225, 85, 85);
+  
+  rectMode(RADIUS);
+  fill(170, 0, 120);
+  rect(225, 225, 75, 75);
+  
+  rectMode(RADIUS);
+  fill(210, 0, 120);
+  rect(225, 225, 65, 65);
+  
+  rectMode(RADIUS);
+  fill(250, 0, 120);
+  rect(225, 225, 55, 55);
+  
+  rectMode(RADIUS);
+  fill(250, 250, 250);
+  rect(225, 225, 45, 45);
+  
+}
